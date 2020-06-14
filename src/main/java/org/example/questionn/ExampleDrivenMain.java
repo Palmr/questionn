89169637d777a32ba738f5c248244dbe9590a737
James Byatt <grumpyjames@gmail.com>
2020-06-14 18:37:46 +0100

    Extract ExampleDrivenMain

---
+++ src/main/java/org/example/questionn/ExampleDrivenMain.java
@@ -0,0 +1,134 @@
+package org.example.questionn;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Collections;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import org.example.questionn.db.DatabaseConfig;
+import org.example.questionn.yaml.YamlLoader;
+import org.h2.tools.Server;
+
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import ratpack.server.RatpackServer;
+
+public class ExampleDrivenMain
+{
+    public static void main(String... args) throws Exception
+    {
+        ArgumentParser parser = ArgumentParsers
+            .newFor("ExampleDrivenMain")
+            .build()
+            .defaultHelp(true)
+            .description("Run a Questionn server based on an example directory");
+        parser.addArgument("-d", "--example-directory")
+            .required(true)
+            .help("The example directory name (e.g 'shopp')");
+        try
+        {
+            final Namespace namespace = parser.parseArgs(args);
+            final String exampleDirectory = namespace.getString("example_directory");
+
+            Path path = Files.createTempDirectory("questionn-eg");
+            Example example = startExample(path.toAbsolutePath().toString(), exampleDirectory);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try
+                {
+                    example.stop();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }));
+
+        }
+        catch (ArgumentParserException e)
+        {
+            parser.handleError(e);
+            System.exit(1);
+        }
+    }
+
+    static final class Example
+    {
+        private final Server server;
+        private final RatpackServer ratpackServer;
+
+        public Example(
+                Server server,
+                RatpackServer ratpackServer)
+        {
+            this.server = server;
+            this.ratpackServer = ratpackServer;
+        }
+
+        public void stop() throws Exception
+        {
+            this.server.stop();
+            this.ratpackServer.stop();
+        }
+    }
+
+    @SuppressFBWarnings(value = "DMI_EMPTY_DB_PASSWORD", justification = "This is only used to run examples")
+    static Example startExample(
+            final String databaseRootPath,
+            final String exampleDirectoryName) throws Exception
+    {
+        final Server server = Server.createTcpServer(
+                "-tcpPort", "8080",
+                "-tcpAllowOthers",
+                "-baseDir", databaseRootPath).start();
+
+        final File exampleDirectory = new File("examples/" + exampleDirectoryName);
+        final File ddl = new File(exampleDirectory, "bootstrap/ddl.sql");
+        final File data = new File(exampleDirectory, "bootstrap/data.sql");
+
+        String databaseUrl = "jdbc:h2:" + databaseRootPath + ";MODE=mysql";
+        try (final Connection conn = DriverManager.getConnection(databaseUrl, "sa", "");
+             final Statement statement = conn.createStatement())
+        {
+            statement.execute("CREATE schema questionn");
+            statement.execute("RUNSCRIPT FROM '" + ddl.getAbsolutePath() + "'");
+            statement.execute("RUNSCRIPT FROM '" + data.getAbsolutePath() + "'");
+        }
+
+        ServerConfiguration serverConfiguration = new ServerConfiguration();
+
+        DatabaseConfig questionnDatabase = new DatabaseConfig();
+        questionnDatabase.url = databaseUrl + ";SCHEMA=questionn";
+        questionnDatabase.username = "sa";
+        questionnDatabase.password = "";
+
+        DatabaseConfig shoppDatabase = new DatabaseConfig();
+        shoppDatabase.url = databaseUrl + ";SCHEMA=" + exampleDirectoryName;
+        shoppDatabase.username = "sa";
+        shoppDatabase.password = "";
+
+        RatpackConfiguration ratpackConfiguration = new RatpackConfiguration();
+        ratpackConfiguration.isDevelopment = true;
+        ratpackConfiguration.port = 8081;
+
+        serverConfiguration.configurationPath = exampleDirectory.getAbsolutePath();
+        serverConfiguration.questionnDatabase = questionnDatabase;
+        serverConfiguration.databasesForQuery = Collections.singletonMap(exampleDirectoryName, shoppDatabase);
+        serverConfiguration.ratpackConfiguration = ratpackConfiguration;
+
+        final RatpackServer ratpackServer = QuestionnMain.startRatpack(
+                YamlLoader.newInstance(),
+                serverConfiguration
+        );
+
+        return new Example(server, ratpackServer);
+    }
+}
--- src/test/java/org/example/questionn/ExampleDrivenAcceptanceTest.java
+++ src/test/java/org/example/questionn/ExampleDrivenAcceptanceTest.java
@@ -2,16 +2,8 @@ package org.example.questionn;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.Collections;
+import java.util.Objects;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-import org.example.questionn.db.DatabaseConfig;
-import org.example.questionn.yaml.YamlLoader;
-import org.h2.tools.Server;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +18,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import ratpack.server.RatpackServer;
 
 public class ExampleDrivenAcceptanceTest
 {
@@ -37,70 +28,21 @@ public class ExampleDrivenAcceptanceTest
     @TempDir
     File databaseRoot;
 
-    private Server server;
-    private RatpackServer ratpackServer;
+    private ExampleDrivenMain.Example example;
 
     @BeforeEach
-    @SuppressFBWarnings(value = "DMI_EMPTY_DB_PASSWORD", justification = "It's a test. Cry me a river.")
     public void startH2() throws Exception
     {
-        this.server = Server.createTcpServer(
-                "-tcpPort", "8080",
-                "-tcpAllowOthers",
-                "-baseDir", databaseRoot.getAbsolutePath()).start();
-        final String exampleDirectoryName = "shopp";
-
-        final File exampleDirectory = new File("examples/" + exampleDirectoryName);
-        final File ddl = new File(exampleDirectory, "bootstrap/ddl.sql");
-        final File data = new File(exampleDirectory, "bootstrap/data.sql");
-
-        String databaseUrl = "jdbc:h2:" + databaseRoot + ";MODE=mysql";
-        try (final Connection conn = DriverManager.getConnection(databaseUrl, "sa", "");
-             final Statement statement = conn.createStatement())
-        {
-            statement.execute("CREATE schema questionn");
-            statement.execute("RUNSCRIPT FROM '" + ddl.getAbsolutePath() + "'");
-            statement.execute("RUNSCRIPT FROM '" + data.getAbsolutePath() + "'");
-        }
-
-        ServerConfiguration serverConfiguration = new ServerConfiguration();
-
-        DatabaseConfig questionnDatabase = new DatabaseConfig();
-        questionnDatabase.url = databaseUrl + ";SCHEMA=questionn";
-        questionnDatabase.username = "sa";
-        questionnDatabase.password = "";
-
-        DatabaseConfig shoppDatabase = new DatabaseConfig();
-        shoppDatabase.url = databaseUrl + ";SCHEMA=" + exampleDirectoryName;
-        shoppDatabase.username = "sa";
-        shoppDatabase.password = "";
-
-        RatpackConfiguration ratpackConfiguration = new RatpackConfiguration();
-        ratpackConfiguration.isDevelopment = true;
-        ratpackConfiguration.port = 8081;
-
-        serverConfiguration.configurationPath = exampleDirectory.getAbsolutePath();
-        serverConfiguration.questionnDatabase = questionnDatabase;
-        serverConfiguration.databasesForQuery = Collections.singletonMap(exampleDirectoryName, shoppDatabase);
-        serverConfiguration.ratpackConfiguration = ratpackConfiguration;
-
-        ratpackServer = QuestionnMain.startRatpack(
-                YamlLoader.newInstance(),
-                serverConfiguration
-        );
+        final String databaseRootPath = databaseRoot.getAbsolutePath();
+        example = ExampleDrivenMain.startExample(databaseRootPath, "shopp");
     }
 
     @AfterEach
     void stopH2() throws Exception
     {
-        if (this.server != null)
+        if (this.example != null)
         {
-            this.server.stop();
-        }
-
-        if (this.ratpackServer != null)
-        {
-            this.ratpackServer.stop();
+            this.example.stop();
         }
     }
 
@@ -114,15 +56,14 @@ public class ExampleDrivenAcceptanceTest
     @SuppressWarnings("SameParameterValue")
     String post(String url, String json) throws IOException
     {
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
                 .url(url)
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute())
         {
-            assert response.body() != null;
-            return response.body().string();
+            return Objects.requireNonNull(response.body()).string();
         }
     }
 }
